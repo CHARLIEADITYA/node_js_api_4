6018090e62630cd7796b133f9b5f1ac30989643e
Aditya Baranwal <95423621+CHARLIEADITYA@users.noreply.github.com>
2024-05-02 11:28:38 +0530

    index.js changes

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get('/nexhealth/:id', async (req, res) => {
 
 app.post('/nexhealth', async (req, res) => {
   const {
-    PATIENT_NAME,CHOOSE_DOC,SPECIALIZATION,AGE,MOB,GENDER,A_DATE,B_GROUP,ADDRESS,CITY,STATE,PINCODE,FEES,DISCOUNT,AMOUNT,DISC_BY} = req.body;
+    PATIENT_NAME, CHOOSE_DOC, SPECIALIZATION, AGE, MOB, GENDER, A_DATE, B_GROUP, ADDRESS, CITY, STATE, PINCODE, FEES, DISCOUNT, AMOUNT, DISC_BY, P_DESCRIBE, S_INSTRUCTION } = req.body;
   try {
     const pool = await mssql.connect(dbconfig);
     const result = await pool
@@ -74,8 +74,12 @@ app.post('/nexhealth', async (req, res) => {
       .input('DISCOUNT', mssql.VarChar, DISCOUNT)
       .input('AMOUNT', mssql.VarChar, AMOUNT)
       .input('DISC_BY', mssql.VarChar, DISC_BY)
-      .query('INSERT INTO REGISTRATION_FORM (PATIENT_NAME, CHOOSE_DOC, SPECIALIZATION, AGE, MOB, GENDER, A_DATE,B_GROUP,ADDRESS,CITY, STATE,PINCODE,FEES,DISCOUNT,AMOUNT,DISC_BY) VALUES (@PATIENT_NAME,@CHOOSE_DOC,@SPECIALIZATION,@AGE,@MOB,@GENDER,@A_DATE,@B_GROUP,@ADDRESS,@CITY, @STATE,@PINCODE,@FEES,@DISCOUNT,@AMOUNT,@DISC_BY)');
-    res.json({ message: 'Data added successfully' }); // Send JSON response
+      .input('P_DESCRIBE', mssql.VarChar, P_DESCRIBE)
+      .input('S_INSTRUCTION', mssql.VarChar, S_INSTRUCTION)
+      .query('INSERT INTO REGISTRATION_FORM (PATIENT_NAME, CHOOSE_DOC, SPECIALIZATION, AGE, MOB, GENDER, A_DATE,B_GROUP,ADDRESS,CITY, STATE,PINCODE,FEES,DISCOUNT,AMOUNT,DISC_BY,P_DESCRIBE,S_INSTRUCTION) VALUES (@PATIENT_NAME,@CHOOSE_DOC,@SPECIALIZATION,@AGE,@MOB,@GENDER,@A_DATE,@B_GROUP,@ADDRESS,@CITY, @STATE,@PINCODE,@FEES,@DISCOUNT,@AMOUNT,@DISC_BY,@P_DESCRIBE,@S_INSTRUCTION)');
+      res.json({
+        data:result
+      });// Send JSON response
   } catch (err) {
     console.error('Error:', err);
     res.status(500).json({ error: 'Something went wrong' }); // Send JSON error response
@@ -85,7 +89,7 @@ app.post('/nexhealth', async (req, res) => {
 // PUT data (update)
 app.put('/nexhealth/:id', async (req, res) => {
   const ID = req.params.id;
-  const { PATIENT_NAME, CHOOSE_DOC, SPECIALIZATION, AGE, MOB, GENDER, A_DATE, B_GROUP, ADDRESS, CITY, STATE, PINCODE, FEES, DISCOUNT, AMOUNT, DISC_BY } = req.body;
+  const { PATIENT_NAME, CHOOSE_DOC, SPECIALIZATION, AGE, MOB, GENDER, A_DATE, B_GROUP, ADDRESS, CITY, STATE, PINCODE, FEES, DISCOUNT, AMOUNT, DISC_BY, P_DESCRIBE, S_INSTRUCTION } = req.body;
   try {
     const pool = await mssql.connect(dbconfig);
     const result = await pool
@@ -107,7 +111,9 @@ app.put('/nexhealth/:id', async (req, res) => {
       .input('DISCOUNT', mssql.VarChar, DISCOUNT)
       .input('AMOUNT', mssql.VarChar, AMOUNT)
       .input('DISC_BY', mssql.VarChar, DISC_BY)
-      .query('UPDATE REGISTRATION_FORM SET PATIENT_NAME = @PATIENT_NAME, CHOOSE_DOC = @CHOOSE_DOC, SPECIALIZATION = @SPECIALIZATION, AGE = @AGE,MOB = @MOB, GENDER = @GENDER, A_DATE = @A_DATE,B_GROUP = @B_GROUP,ADDRESS = @ADDRESS, CITY = @CITY, STATE = @STATE, PINCODE = @PINCODE, FEES = @FEES, DISCOUNT = @DISCOUNT, AMOUNT = @AMOUNT, DISC_BY = @DISC_BY WHERE ID = @id');
+      .input('P_DESCRIBE', mssql.VarChar, P_DESCRIBE)
+      .input('S_INSTRUCTION', mssql.VarChar, S_INSTRUCTION)
+      .query('UPDATE REGISTRATION_FORM SET PATIENT_NAME = @PATIENT_NAME, CHOOSE_DOC = @CHOOSE_DOC, SPECIALIZATION = @SPECIALIZATION, AGE = @AGE,MOB = @MOB, GENDER = @GENDER, A_DATE = @A_DATE,B_GROUP = @B_GROUP,ADDRESS = @ADDRESS, CITY = @CITY, STATE = @STATE, PINCODE = @PINCODE, FEES = @FEES, DISCOUNT = @DISCOUNT, AMOUNT = @AMOUNT, DISC_BY = @DISC_BY,P_DESCRIBE = @P_DESCRIBE,S_INSTRUCTION = @S_INSTRUCTION WHERE ID = @id');
     console.log("qry", result)
     res.json({ message: 'Data update successfully' }); // Send JSON response
   } catch (err) {
@@ -132,45 +138,155 @@ app.delete('/nexhealth/:id', async (req, res) => {
   }
 });
 
-   // Doctor
-   app.get('/doctor', async (req, res) => {
-    try {
-      const pool = await mssql.connect(dbconfig);
-      const result = await pool.request().query('SELECT * FROM DOCTOR_ID');
-      res.json(result.recordset);
-    } catch (err) {
-      console.error('Error:', err);
-      res.status(500).send('Something went wrong');
-    }
-  });
-
-  app.post('/login', async (req, res) => {
-    const { DOCTOR_ID, DOC_PASS } = req.body;
-
-    try {
-        // Connect to the database
-        const pool = await mssql.connect(dbconfig);
-
-        // Query the database to find the user
-        const result = await pool.request()
-            .input('DOCTOR_ID', mssql.VarChar, DOCTOR_ID)
-            .input('DOC_PASS', mssql.VarChar, DOC_PASS)
-            .query('SELECT * FROM DOCTOR_ID WHERE DOCTOR_ID = @DOCTOR_ID AND DOC_PASS = @DOC_PASS');
-
-        // If user exists, return success message
-        if (result.recordset.length > 0) {
-            res.json({ message: 'Login successful' });
-        } else {
-            res.status(401).json({ message: 'Invalid username or password.' });
-        }
-    } catch (err) {
-        console.error('SQL Error:', err.message);
-        res.status(500).json({ message: 'Internal server error' });
-    }
+// Doctor
+app.get('/login', async (req, res) => {
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool.request().query('SELECT * FROM doctor_id');
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
 });
 
+app.get('/login/:id', async (req, res) => {
+  const D_ID = req.params.id;
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool
+      .request()
+      .input('D_ID', mssql.Int, D_ID)
+      .query('SELECT * FROM doctor_id WHERE D_ID = @D_ID');
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { DOCTOR_ID, DOC_PASS } = req.body;
+
+  try {
+    // Connect to the database
+    const pool = await mssql.connect(dbconfig);
+    // Query the database to find the user
+    const result = await pool.request()
+      .input('DOCTOR_ID', mssql.VarChar, DOCTOR_ID)
+      .input('DOC_PASS', mssql.VarChar, DOC_PASS)
+      .query('SELECT * FROM doctor_id WHERE DOCTOR_ID = @DOCTOR_ID AND DOC_PASS = @DOC_PASS');
+    
+    // If user exists, return success message
+    if (result.recordset.length > 0) {
+      res.json({
+        data: result.recordset
+      });
+    }
+  } catch (err) {
+    console.error('SQL Error:', err.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+//  Tablets
+
+app.get('/tablets', async (req, res) => {
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool.request().query(`select DRUG_NAME FROM DRUG_DATA WHERE DRUG_TYPE = 'TAB' `);
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
+app.get('/Syrup', async (req, res) => {
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool.request().query(`select DRUG_NAME FROM DRUG_DATA WHERE DRUG_TYPE = 'SYRUP' `);
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
+app.get('/Injection', async (req, res) => {
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool.request().query(`select DRUG_NAME FROM DRUG_DATA WHERE DRUG_TYPE = 'Injection' `);
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
+app.get('/CAP', async (req, res) => {
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool.request().query(`select DRUG_NAME FROM DRUG_DATA WHERE DRUG_TYPE = 'CAP' `);
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+// Medicine add api
+
+app.post('/medical/:id', async (req, res) => {
+  const ID = req.params.id;
+  const { M_TYPE, M_Name, DOSE, DURATION
+  } = req.body;
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool
+      .request()
+      .input('ID', mssql.Int, ID)
+      .input('M_TYPE', mssql.VarChar, M_TYPE)
+      .input('M_Name', mssql.VarChar,M_Name)
+      .input('DOSE', mssql.VarChar, DOSE)
+      .input('DURATION', mssql.VarChar, DURATION)
+      .query(`INSERT INTO MEDICINE_TABLE (M_TYPE, M_Name, DOSE, DURATION,ID)
+      VALUES (@M_TYPE,@M_Name,@DOSE,@DURATION,@ID)`)
+    res.json({ message: 'Data added successfully' }); // Send JSON response
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ error: 'Something went wrong' }); // Send JSON error response
+  }
+});
+
+app.get('/medical/:id', async (req, res) => {
+  const ID = req.params.id;
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool
+      .request()
+      .input('ID', mssql.Int, ID)
+      .query('SELECT * FROM MEDICINE_TABLE WHERE ID= @ID')
+    res.json(result.recordset);
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+app.delete('/medical/:id', async (req, res) => {
+  const M_Id = req.params.id;
+  try {
+    const pool = await mssql.connect(dbconfig);
+    const result = await pool
+      .request()
+      .input('M_Id', mssql.Int, M_Id)
+      .query('DELETE FROM MEDICINE_TABLE WHERE M_Id = @M_Id');
+    res.json({ message: 'Data delete successfully' }); // Send JSON response
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).send('Something went wrong');
+  }
+});
 
 // Start server
-app.listen(4000, () => {
-  console.log(`Server is running on port`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${port}`);
 });
